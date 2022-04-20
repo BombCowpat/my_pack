309237f439ca2ce91c02c9dfad78bc5dbf19b6aa
chenyanfei <15873624283@163.com>
2022-04-20 11:53:16 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import {sum, square} from './js/utils.js'
+console.log(sum(10,20))
+console.log(square(10))
---
+++ src/js/utils.js
@@ -0,0 +1,8 @@
+export const sum = (m, n) => {
+  return m + n
+}
+
+export const square = (m) => {
+  return m * m
+}
+
